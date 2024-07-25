f925c8e35f5026a5de17251aa16afa0a4010acc5
dldudcks91 <dldudcks91@gmail.com>
2024-07-26 02:22:37 +0900

    add to crontab file

---
+++ bs_crontab/crawling_start_game.py
@@ -0,0 +1,86 @@
+#%%
+
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+#%%
+import requests
+from bs4 import BeautifulSoup 
+import json
+import time
+import pandas as pd
+import numpy as np
+
+from datetime import datetime, timedelta
+
+import pymysql
+
+from bs_crawling import base as cb
+from bs_personal import personal_code as cd
+from bs_crawling import kbo_request as kr
+
+
+
+ck = kr.Crawling_kbo_request()
+conn_aws = pymysql.connect(host = cd.aws_host, user=cd.aws_user, password= cd.aws_code, db= cd.db, charset='utf8')
+conn = conn_aws
+ck.set_last_game_num_list(ck.year,conn)
+conn.close()
+
+
+date_str = str(datetime.today().date()).replace("-","")
+date = int(date_str)
+ck.year = int(date_str[:4])
+ck.craw_game_info(date)
+ck.craw_lineup(date)
+ck.set_date_start(date)
+#%%
+conn_aws = pymysql.connect(host = cd.aws_host, user=cd.aws_user, password= cd.aws_code, db= cd.db, charset='utf8')
+conn = conn_aws
+try:
+    ck.delete_table_data(conn, 'today_lineup')
+    ck.delete_table_data(conn, 'today_team_game_info')
+    ck.delete_table_data(conn, 'today_game_info')
+    
+    conn.commit()
+    print(f"Success Delete start_game_data")
+except Exception as e:
+    conn.rollback()
+    print(f"InterfaceError 발생: {e}")
+except Exception as e:
+    print(f"다른예외처리: {e}")
+finally:
+    
+    conn.close()
+#%%
+conn_aws = pymysql.connect(host = cd.aws_host, user=cd.aws_user, password= cd.aws_code, db= cd.db, charset='utf8')
+conn = conn_aws
+try:
+    ck.array_to_db(conn, ck.game_info_array, 'today_game_info')  
+    ck.array_to_db(conn, ck.team_game_info_array, 'today_team_game_info')
+    ck.array_to_db(conn, ck.lineup_array, 'today_lineup')
+    
+    conn.commit()
+    print(f"Success insert start_game_data")
+except pymysql.InterfaceError as e:
+    conn.rollback()
+    print(f"InterfaceError 발생: {e}")
+except Exception as e:
+    print(f"다른예외처리: {e}")
+    
+finally:
+    conn.close()
+
+
+
+
+
+
+
+
+
+
+
+
